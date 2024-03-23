9f0061461e69b26e5acc3859152616143bf0a052
Mujtaba <149222185+Mujtabasiraj@users.noreply.github.com>
2024-03-23 15:51:18 +0530

    Add files via upload

---
+++ TypeCast.java
@@ -0,0 +1,16 @@
+class TypeCast {
+    
+    public static void main(String[] args){
+        int marks=80;
+        long lmv=marks;
+
+        System.out.println("integer value"+marks);
+        System.out.println("long value"+lmv);
+        double d= 129.345;
+        double inm=(double)d;
+
+        System.out.println("double value"+d);
+        System.out.println("integer value"+lmv);
+    }
+    
+}
